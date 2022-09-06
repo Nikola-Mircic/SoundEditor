4798d0aa8ecc549442d1c8d3c4449a2b7694b24f
Nikola-Mircic <nikola123mircic@gmail.com>
2022-09-06 22:20:03 +0200

    Created class for audio player

---
+++ src/audio/Player.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <iostream>
+#include <future>
+
+#include <SDL2/SDL.h>
+
+#include "WAVReader.h"
+
+#define RUN(CMD) if(!CMD) { \
+                    printf("|\n| [%d] PortAudio error: %s\n|\n", __LINE__, SDL_GetError()); \
+                    return; \
+                 }
+
+struct AudioData{
+    SDL_AudioSpec* spec;
+
+    Uint8* dataPos;
+    Uint8* currentPos;
+
+    Uint32 dataLength;
+    Uint32 currentLength;
+};
+
+class Player{
+public:
+    Player();
+
+    ~Player();
+
+    void playSound(const char* filePath);
+    void stopSound();
+    void restartSound();
+    void continueSound();
+
+    static void handleSound(bool* playing, AudioData* sound);
+    void loadData();
+private:
+    AudioData* soundData;
+    const char* filePath;
+    bool playing;
+
+    std::future<void> futureCall;
+};
