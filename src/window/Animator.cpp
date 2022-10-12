a9852fc09efe3f9ab722071e8f0182e5f1f9f06d
Nikola-Mircic <nikola123mircic@gmail.com>
2022-10-12 13:40:57 +0200

    Added spectrogram animation

--- src/window/Animator.cpp
+++ src/window/Animator.cpp
@@ -2,6 +2,10 @@
 
 #include <cmath>
 
+#include <chrono>
+
+#define wxWS_EX_BUFFERED_DRAW 0x00000040
+
 Animator::Animator(wxWindow* window){
     this->window = window;
 
@@ -12,29 +16,40 @@ Animator::~Animator(){
 
 }
 
-void Animator::SetData(double value, double freq){
-    std::lock_guard<std::mutex> dataGuard(dataMutex);
-    
-    this->value = value;
-    this->freq = freq;
+void Animator::TimerStart(){
+    Start(50);
 }
 
-void Animator::TimerStart(){
-    Start(30);
-}
+double maxTime = 0;
 
 void Animator::Notify(){
+    auto start = std::chrono::system_clock::now();
     Draw();
+
+    auto end = std::chrono::system_clock::now();
+
+    std::chrono::duration<double> elapsed_seconds = end-start;
+
+    double secs = elapsed_seconds.count();
+
+    if(secs > maxTime){
+        maxTime = secs;
+       /* std::cout << secs << "\n";*/
+    }
 }
 
 void Animator::ActivateRenderLoop(bool on){
     if(on && !rendering)
     {   
+        panel = new wxPanel(window, wxID_ANY, {20, 350}, {300, 350});
+        panel->Freeze();
+        //panel->SetExtraStyle(wxWS_EX_BUFFERED_DRAW);
         TimerStart();
         rendering = true;
     }
     else if(!on && rendering)
     {   
+        tslist.clearAll();
         Stop();
         rendering = false;
     }
@@ -44,22 +59,86 @@ double last_r=0;
 
 void Animator::Draw(){
     if(rendering){
-        int r = std::abs(value/40);
+        static wxClientDC dc(panel);
 
-        int green = 150+freq*25;
+        static int width = 2;
+        
+        wxColor bckg = window->GetBackgroundColour();
+        bckg.Set(bckg.Red(), bckg.Green(), bckg.Blue(), wxALPHA_OPAQUE);
+        dc.SetBrush(wxBrush(bckg));
+        dc.SetPen(wxPen(bckg, 3));
+        dc.DrawRectangle(50, 0, 200, 50);
+
+        tslist.forEach([](sample s, int idx){
+            int height = std::min(50, (int)(600*val(s)/__SHRT_MAX__));
+
+            int green = 150+freq(s)*25;
+            int blue = std::min(255, std::max(green-255, 0));
+            green = std::min(green, 255);
+            
+            dc.SetBrush(wxBrush(wxColor(0, green, blue)));
+            dc.SetPen(wxPen(wxColor(0, green, blue)));
+            dc.DrawRectangle(50+idx*width, 50-height, width, height);
+        });
+
+        sample s = tslist.back();
+        int r = std::min(150, (int) std::abs(val(s)/40));
+
+        int green = 150+freq(s)*25;
         int blue = std::min(255, std::max(green-255, 0));
         green = std::min(green, 255);
 
-        wxClientDC dc(window);
-
         dc.SetBrush(wxBrush(window->GetBackgroundColour()));
         dc.SetPen(wxPen(window->GetBackgroundColour(), 3));
-        dc.DrawCircle(100, 500, last_r);
+        dc.DrawCircle(150, 200, last_r);
 
         dc.SetBrush(wxBrush(wxColor(0, green, blue)));
         dc.SetPen(wxPen(wxColor(0, green, blue)));
-        dc.DrawCircle(100, 500, r);
+        dc.DrawCircle(150, 200, r);
 
         last_r = r;
     }
+}
+
+void Animator::TSList::push(sample& s){
+    std::lock_guard<std::mutex> dataGuard(dataMutex);
+
+    if(samples.size() >= 100)
+        samples.pop_front();
+
+    samples.push_back(s);
+}
+
+void Animator::TSList::remove_front(){
+    std::lock_guard<std::mutex> dataGuard(dataMutex);
+
+    samples.pop_front();
+}
+
+void Animator::TSList::clearAll(){
+    std::lock_guard<std::mutex> dataGuard(dataMutex);
+
+    samples.clear();
+}
+
+sample& Animator::TSList::back(){
+    std::lock_guard<std::mutex> dataGuard(dataMutex);
+
+    return samples.back();
+}
+
+void Animator::TSList::forEach(void (*func)(sample, int)){
+    std::lock_guard<std::mutex> dataGuard(dataMutex);
+
+    std::list<sample>::iterator it;
+    int idx = 0;
+
+    for (it = samples.begin(); it != samples.end(); ++it)
+        func(*it, idx++);
+}
+
+size_t Animator::TSList::size(){
+    std::lock_guard<std::mutex> dataGuard(dataMutex);
+
+    return samples.size();
 }
--- src/window/Animator.h
+++ src/window/Animator.h
@@ -1,33 +1,52 @@
 #pragma once
 
 #include <iostream>
-#include <deque>
+#include <iterator>
+#include <list>
 #include <mutex>
 
 #include "wx/wx.h"
+#include <wx/dcbuffer.h>
 
-typedef std::pair<double, double> sample;
+#define val(s) (s).first // Macro for sample value
+#define freq(s) (s).second // Macro for sample freq
+
+typedef std::pair<double, double> sample; // Sample definition
+
+
 
 class Animator : public wxTimer{
 public:
     Animator(wxWindow* window);
     ~Animator();
 
-    void SetData(double value, double freq);
     void ActivateRenderLoop(bool on);
 
     void Notify();
     void TimerStart();
 
     void Draw();
+
+    struct TSList{
+        std::mutex dataMutex;
+
+        std::list<sample> samples;
+
+        void push(sample& s);
+
+        void remove_front();
+
+        void clearAll();
+
+        sample& back();
+
+        void forEach(void (*func)(sample, int));
+
+        size_t size();
+    } tslist;
+
 private:
-    std::mutex dataMutex;
-
-    double value;
-    double freq;
-
-    std::deque<sample> samples;
-
+    wxPanel* panel;
     wxWindow* window;
     bool rendering = false;
 };
